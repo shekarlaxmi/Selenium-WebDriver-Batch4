c5ac7e8d374dc81015146d5869260c5c053f3aaf
shekarlaxmi <30628900+shekarlaxmi@users.noreply.github.com>
2017-08-03 06:58:02 +0530

    Update BookingConfirmationPage.java

--- SeleniumWebDriver/src/pageLibrary/BookingConfirmationPage.java
+++ SeleniumWebDriver/src/pageLibrary/BookingConfirmationPage.java
@@ -10,7 +10,7 @@ public class BookingConfirmationPage {
 		this.driver = driver;
 	}
 	
-	
+	//get message
 	public String getConfirmationMessage(){
 		String msg = driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr[1]/td[2]/table/tbody/tr[3]/td/p/font/b/font[2]")).getText();
 		return msg;
